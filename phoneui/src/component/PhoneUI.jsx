1a44cd49a4025b1c7867894bfd6b903d65e28327
Agada Unogwu <unogwuagada@gmail.com>
2025-12-15 02:01:08 -0800

    little change

--- phoneui/src/component/PhoneUI.jsx
+++ phoneui/src/component/PhoneUI.jsx
@@ -1,8 +1,10 @@
 import "./phoneUI.css"
 const PhoneUI = () => {
+
     return (
         <div className="main">
 
+            {/* Layer 1 div */}
             <div className="layer1">
                 <div className="bar">
                     <p>4:20</p>
@@ -24,7 +26,39 @@ const PhoneUI = () => {
                     </div>
 
                     <div className="bar-left">
-                        <span><ion-icon name="notifications-circle-outline"></ion-icon></span>
+                        <span className="bar-left-main"><ion-icon name="notifications-circle-outline"></ion-icon></span>
+                        <span className="red-dot"></span>
+                    </div>
+                </div>
+            </div>
+
+            {/* Layer 2 div --- Amount Cards */}
+            <div className="layer2">
+                <div className="layer2main">
+                    <div className="card1">
+                        <span className="c1"><ion-icon name="notifications"></ion-icon></span>
+                        <p>Total Payout</p>
+                        <h2>N40,000</h2>
+                    </div>
+
+                    <div className="card1">
+                        <span className="c2"><ion-icon name="notifications"></ion-icon></span>
+                        <p>Total Trades</p>
+                        <h2>10</h2>
+                    </div>
+                </div>
+
+                <div className="layer2main2" >
+                    <div className="card1">
+                        <span className="c3"><ion-icon name="notifications"></ion-icon></span>
+                        <p>Completed Trades</p>
+                        <h2>8</h2>
+                    </div>
+
+                    <div className="card1">
+                        <span className="c4"><ion-icon name="notifications"></ion-icon></span>
+                        <p>Failed Trades</p>
+                        <h2>2</h2>
                     </div>
                 </div>
             </div>
